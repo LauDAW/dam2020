a4cbc9bb76a9c9b914b4d1770559557f14a726b2
Laura <eleh93@gmail.com>
2020-10-16 12:19:33 +0200

    Programa1.py añadido

---
+++ programa1.py
@@ -0,0 +1,6 @@
+print("== Media aritmética ==")
+num1 = int(input("Introduzca el primer número: "))
+num2 = int(input("Introduzca el segundo número: "))
+media_aritmetica = ((num1 + num2) / 2)
+
+print("El valor de la media aritmética de " + str(num1) + " y " + str(num2) + " es " + str(media_aritmetica))
