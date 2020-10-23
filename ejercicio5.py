e9ffcd0c88acc8c5c9b81d2fd8eeb45e81565c21
LauDAW <44204397+LauDAW@users.noreply.github.com>
2020-10-23 21:32:44 +0200

    Añadido ejercicio10.py

---
+++ ejercicio5.py
@@ -0,0 +1,12 @@
+print("== Convertir binarios en enteros ==")
+
+binario = int(input("Introduce un número binario para transformar: "))
+
+def binario_a_entero(binario):
+    decimal = 0
+    binario = str(binario)
+    decimal = int(binario, 2)
+
+    return decimal
+
+print("El número binario", binario, "equivale a", binario_a_entero(binario))
